0b47a5855f53f0e273618c4f7c72f3e28c8d7568
Ethan Devenport <edevenport@users.noreply.github.com>
2018-07-18 19:43:23 +0300

    Initial commit
    Signed-off-by: Ethan Devenport <edevenport@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,87 @@
+## Getting Started
+
+The following will describe how to install and configure the DigitalOcean blockstore plugin for Ark and provide a usage example.
+
+### Prerequisites
+
+* Kubernetes cluster
+* DigitalOcean account and resources
+  * [API personal access token](https://www.digitalocean.com/docs/api/create-personal-access-token/)
+  * [Spaces access keys](https://www.digitalocean.com/docs/spaces/how-to/administrative-access/)
+  * Spaces bucket
+  * Spaces bucket region
+* [Heptio Ark](https://heptio.github.io/ark/master/quickstart.html) prerequisites.
+
+### Install the plugin
+
+1. Complete the Heptio Ark prerequisites mentioned above. This generally involves apply the `00-prereqs.yaml` available in the Ark repository:
+
+    ```
+    kubectl apply -f 00-prereqs.yaml
+    ```
+
+2. Update the `examples/credentials-ark` with your Spaces access and secret keys. The file will look like the following:
+
+    ```
+    [default]
+    aws_access_key_id=<AWS_ACCESS_KEY_ID>
+    aws_secret_access_key=<AWS_SECRET_ACCESS_KEY>
+    ```
+
+3. Create a Kubernetes `cloud-credentials` secret containing the `credentials-ark` and DigitalOcean API token.
+
+    ```
+    kubectl create secret generic cloud-credentials \
+        --namespace heptio-ark \
+        --from-file cloud=credentials-ark \
+        --from-literal digitalocean_token=<DIGITALOCEAN_TOKEN>
+    ```
+
+4. Update the `example/10-ark-config.yaml` with the Spaces API URL, bucket, and region and apply the configuration:
+
+    ```
+    kubectl apply -f 10-ark-config.yaml
+    ```
+
+5. Now apply the Ark deployment.
+
+    ```
+    kubectl apply -f 20-deployment.yaml
+    ```
+
+6. Finally add the `ark-blockstore-digitalocean` plugin to Ark.
+
+    ```
+    ark plugin add quay.io/stackpoint/ark-blockstore-digitalocean:latest
+    ```
+
+### Backup and restore example
+
+1. First apply the example Nginx config that uses persistent storage for the log path.
+
+    ```
+    kubectl apply -f examples/nginx-pv.yml
+    ```
+
+2. Once `nginx-example` is running and available, create a backup using Ark.
+
+    ```
+    ark backup create nginx-backup --selector app=nginx
+    ark backup describe nginx-backup
+    ```
+
+3. The config files should appear in the Spaces bucket and a snapshot taken of the persistent volume. Now you can simulate a disaster by deleting the `nginx-example` namespace.
+
+    ```
+    kubectl delete namespace nginx-example
+    ```
+
+4. The `nginx-data` backup can now be restored.
+
+    ```
+    ark restore create --from-backup nginx-data
+    ```
+
+### Build container
+
+make container IMAGE=quay.io/stackpoint/ark-blockstore-digitalocean
---
+++ ark-blockstore-digitalocean/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/digitalocean/godo"
+	"github.com/heptio/ark/pkg/cloudprovider"
+	"github.com/heptio/ark/pkg/plugin"
+	"github.com/heptio/ark/pkg/util/collections"
+	"github.com/satori/uuid"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type BlockStore struct {
+	client *godo.Client
+	log    logrus.FieldLogger
+}
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func main() {
+	impl := &BlockStore{log: plugin.NewLogger()}
+	plugin.Serve(plugin.NewBlockStorePlugin(impl))
+}
+
+func NewBlockStore() cloudprovider.BlockStore {
+	return &BlockStore{}
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+
+	return token, nil
+}
+
+func (b *BlockStore) Init(config map[string]string) error {
+	b.log.Infof("BlockStore.Init called")
+
+	tokenSource := &TokenSource{
+		AccessToken: os.Getenv("DIGITALOCEAN_TOKEN"),
+	}
+
+	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+	b.client = godo.NewClient(oauthClient)
+
+	return nil
+}
+
+func (b *BlockStore) CreateVolumeFromSnapshot(snapshotID string, volumeType string, volumeAZ string, iops *int64) (volumeID string, err error) {
+	ctx := context.TODO()
+
+	snapshot, _, err := b.client.Storage.GetSnapshot(ctx, snapshotID)
+	if err != nil {
+		b.log.Errorf("Storage.GetSnapshot returned error: %v", err)
+	}
+
+	diskSize := snapshot.MinDiskSize
+
+	createRequest := &godo.VolumeCreateRequest{
+		Name:          "restore-" + uuid.NewV4().String(),
+		SnapshotID:    snapshotID,
+		SizeGigaBytes: int64(diskSize),
+	}
+
+	newVolume, _, err := b.client.Storage.CreateVolume(ctx, createRequest)
+	if err != nil {
+		b.log.Errorf("Storage.CreateVolume returned error: %v", err)
+	}
+
+	return newVolume.ID, nil
+}
+
+func (b *BlockStore) GetVolumeInfo(volumeID string, volumeAZ string) (string, *int64, error) {
+	ctx := context.TODO()
+
+	volume, _, err := b.client.Storage.GetVolume(ctx, volumeID)
+	if err != nil {
+		b.log.Errorf("Storage.GetVolumeInfo returned error: %v", err)
+	}
+
+	return volume.FilesystemType, nil, nil
+}
+
+func (b *BlockStore) IsVolumeReady(volumeID string, volumeAZ string) (bool, error) {
+	return true, nil
+}
+
+func (b *BlockStore) CreateSnapshot(volumeID string, volumeAZ string, tags map[string]string) (string, error) {
+	createRequest := &godo.SnapshotCreateRequest{
+		VolumeID:    volumeID,
+		Name:        "pvs-" + volumeID,
+		Description: "Ark snapshot of pv-" + volumeID,
+	}
+
+	ctx := context.TODO()
+
+	newSnapshot, _, err := b.client.Storage.CreateSnapshot(ctx, createRequest)
+	if err != nil {
+		b.log.Errorf("Storage.CreateSnapshot returned error: %v", err)
+	}
+
+	return newSnapshot.ID, nil
+}
+
+func (b *BlockStore) DeleteSnapshot(snapshotID string) error {
+	ctx := context.TODO()
+
+	_, err := b.client.Storage.DeleteSnapshot(ctx, snapshotID)
+	if err != nil {
+		b.log.Errorf("Storage.DeleteSnapshot returned error: %v", err)
+	}
+
+	return err
+}
+
+func (b *BlockStore) GetVolumeID(pv runtime.Unstructured) (string, error) {
+	if !collections.Exists(pv.UnstructuredContent(), "spec.csi") {
+		return "", nil
+	}
+
+	volumeID, err := collections.GetString(pv.UnstructuredContent(), "spec.csi.volumeHandle")
+	if err != nil {
+		return "", err
+	}
+
+	return volumeID, nil
+}
+
+func (b *BlockStore) SetVolumeID(pv runtime.Unstructured, volumeID string) (runtime.Unstructured, error) {
+	do, err := collections.GetMap(pv.UnstructuredContent(), "spec.csi")
+	if err != nil {
+		b.log.Error("Could not find pv data in UnstructuredContent")
+		return nil, err
+	}
+
+	do["volumeHandle"] = volumeID
+
+	return pv, nil
+}
